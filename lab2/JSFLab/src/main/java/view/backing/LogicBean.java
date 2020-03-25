1206b85366a6e0f056f7ba03527a986689b16809
margohanyz <42206069+margohanyz@users.noreply.github.com>
2020-03-25 21:52:40 +0100

    upload JSFLab

---
+++ lab2/JSFLab/src/main/java/view/backing/LogicBean.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.backing;
+
+import java.util.ResourceBundle;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlSelectBooleanCheckbox;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+
+/**
+ *
+ * @author pawel
+ */
+@Named(value = "logicBean")
+@RequestScoped
+public class LogicBean {
+
+    private String username, password;
+    private HtmlSelectBooleanCheckbox acceptCheckbox;
+    private HtmlCommandButton loginButton;
+
+    /**
+     * Creates a new instance of LogicBean
+     */
+    public LogicBean() {
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+        if (username.equals("scott") && password.equals("tiger")) {
+                FacesContext context = FacesContext.getCurrentInstance();
+                ResourceBundle bundle = ResourceBundle.getBundle("ApplicationMessages");
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, bundle.getString("warn.tiger"), ""));
+                return "index";
+            }
+        else {
+            if(username.equals(password)){
+                return "success";
+            }
+            else
+                return "faliure";
+        }
+    }
+
+    /**
+     * @return the acceptCheckbox
+     */
+    public HtmlSelectBooleanCheckbox getAcceptCheckbox() {
+        return acceptCheckbox;
+    }
+
+    /**
+     * @param acceptCheckbox the acceptCheckbox to set
+     */
+    public void setAcceptCheckbox(HtmlSelectBooleanCheckbox acceptCheckbox) {
+        this.acceptCheckbox = acceptCheckbox;
+    }
+
+    /**
+     * @return the loginButton
+     */
+    public HtmlCommandButton getLoginButton() {
+        return loginButton;
+    }
+
+    /**
+     * @param loginButton the loginButton to set
+     */
+    public void setLoginButton(HtmlCommandButton loginButton) {
+        this.loginButton = loginButton;
+    }
+
+    public void activateButton(ValueChangeEvent e) {
+        if (acceptCheckbox.isSelected()) {
+            loginButton.setDisabled(false);
+
+        } else {
+            loginButton.setDisabled(true);
+        }
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.renderResponse();
+    }
+}
